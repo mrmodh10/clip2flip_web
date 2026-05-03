8a3e14ba7096a6ed2336d3b74503b30c0d61eebc
mohitmodh <mohitmodh10@gmail.com>
2026-05-03 19:52:57 -0400

    Add comment to publicAsset utility

--- src/lib/public-assets.ts
+++ src/lib/public-assets.ts
@@ -1,5 +1,6 @@
 export const PUBLIC_ASSET_BASE_PATH = process.env.NODE_ENV === "production" ? "/clip2flip_web" : "";
 
+// Utility function to handle asset paths for GitHub Pages deployment
 export function publicAsset(path: string) {
   return `${PUBLIC_ASSET_BASE_PATH}${path}`;
 }
