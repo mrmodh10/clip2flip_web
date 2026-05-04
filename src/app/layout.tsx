44739f4e55fabb2c86ab176cae7c93845a382f58
Raj Lalakiya <mailto:raj.lalkiya@logisticinfotech.co.in>
2026-05-04 16:40:48 +0530

    fix(home-page): remove extra whitespace and update Our Services section images to match Figma design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${manrope.variable} ${inter.variable}`}>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
