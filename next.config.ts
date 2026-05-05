5a3ecec33130d5f6786e440498f75c6147882608
mohitmodh <mohitmodh10@gmail.com>
2026-05-05 10:13:54 -0400

    support netlify

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/clip2flip_web" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/clip2flip_web/" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
